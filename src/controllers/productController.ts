8b084d797c1b888666502f613986ba322d17f0da
oleh zelenyi <oleh.zelenyii@gmail.com>
2023-07-15 20:58:00 -0600

    add productService, and productModel. Refactor productController

--- src/controllers/productController.ts
+++ src/controllers/productController.ts
@@ -1,13 +1,30 @@
 import { Request, Response } from "express";
+import { IProduct } from "../models/product";
+import productService from "../services/productService";
 
 export class ProductController {
   getAll = async (req: Request, res: Response) => {
-    const products: any = [];
+    const { limit, skip, sort } = req.params;
+    let products: IProduct[] = [];
+
+    if (limit && skip && sort) {
+      products = await productService.getAll(+limit, +skip, sort);
+    }
+
+    products = await productService.getAll();
+
     return res.status(200).send({ products });
   };
 
   getById = async (req: Request, res: Response) => {
-    const product = undefined;
+    const { id } = req.params;
+
+    if (!id) return res.status(422);
+
+    const product = await productService.getById(id);
+
+    if (!product) return res.status(404).send({ errorMsg: "No product found" });
+
     return res.status(200).send({ product });
   };
 }
