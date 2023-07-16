6784d4f15a58b43d6abbc3fb855a9bb42536504f
oleh zelenyi <oleh.zelenyii@gmail.com>
2023-07-15 20:59:39 -0600

    Revert "add productService, and productModel. Refactor productController"
    This reverts commit 8b084d797c1b888666502f613986ba322d17f0da.

--- src/controllers/productController.ts
+++ src/controllers/productController.ts
@@ -1,30 +1,13 @@
 import { Request, Response } from "express";
-import { IProduct } from "../models/product";
-import productService from "../services/productService";
 
 export class ProductController {
   getAll = async (req: Request, res: Response) => {
-    const { limit, skip, sort } = req.params;
-    let products: IProduct[] = [];
-
-    if (limit && skip && sort) {
-      products = await productService.getAll(+limit, +skip, sort);
-    }
-
-    products = await productService.getAll();
-
+    const products: any = [];
     return res.status(200).send({ products });
   };
 
   getById = async (req: Request, res: Response) => {
-    const { id } = req.params;
-
-    if (!id) return res.status(422);
-
-    const product = await productService.getById(id);
-
-    if (!product) return res.status(404).send({ errorMsg: "No product found" });
-
+    const product = undefined;
     return res.status(200).send({ product });
   };
 }
